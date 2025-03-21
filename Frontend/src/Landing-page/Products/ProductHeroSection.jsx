dc553394e4dfbe31bcbc689994e3127d3d634a5b
Vikramaditya <vikramadityagorai@gmail.com>
2025-03-21 19:07:17 +0530

    product page

--- Frontend/src/Landing-page/Products/ProductHeroSection.jsx
+++ Frontend/src/Landing-page/Products/ProductHeroSection.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 function ProductHeroSection() {
   return (
-    <div className="container text-center mt-5 mb-5">
+    <div className="container text-center mt-5 mb-10 border-bottom">
       <h1>Zerodha Products</h1>
       <h3 className="text-muted mt-3 fs-4">
         Sleek, modern, and intuitive trading platforms
       </h3>
-      <p className="mt-3">
+      <p className="mt-3 mb-5">
         Check out our <a href="">investment offerings →</a>
       </p>
     </div>
